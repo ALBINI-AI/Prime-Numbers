d5ae0cc1f2ff60c14eafa646594c24355028d98c
ALBINI-AI <106315587+ALBINI-AI@users.noreply.github.com>
2022-08-21 23:46:32 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int n1,n2,i,j,temp;
+    printf("Enter the numbers to check:");
+    scanf("%d %d",&n1,&n2);
+    printf("The prime numbers between the given numbers are:\n");
+    for(i=n1;i<=n2;i++)
+    {
+        if(i==2)
+        {
+            continue;
+        }
+        if(i%2==0)
+            continue;
+        temp=1;
+        for(j=2;j<sqrt(i);j++)
+        {
+            if(i%j==0)
+            {
+                temp=0;
+                break;
+            }
+        }
+        if(1==temp){
+            printf("%d ",i);}
+            
+    }
+    return 0;
+}
